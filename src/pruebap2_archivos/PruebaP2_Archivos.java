74cd7b85e5b7f02968cf3ff8f1c4a9337a567602
GalLu707 <zasseuwu@gmail.com>
2026-05-28 14:56:04 -0600

    repo creado

---
+++ src/pruebap2_archivos/PruebaP2_Archivos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebap2_archivos;
+
+/**
+ *
+ * @author USER
+ */
+public class PruebaP2_Archivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
